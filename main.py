1c520534a6c5eb096494b282fe3106e9f9d34bfe
Mateus <160790022+Mateusssssssssss@users.noreply.github.com>
2024-09-06 11:20:28 -0300

    Banco de dados
    estudo sobre banco de dados

---
+++ main.py
@@ -0,0 +1,19 @@
+from os import path
+import sqlite3
+
+from pathlib import Path
+
+ROOT_DIR = Path(__file__).parent
+DB_NAME = 'db.sqlite3'
+#arquivo a ser usado DB_FILE
+DB_FILE = ROOT_DIR / DB_NAME
+
+#Conexão, DB_FILE arquivo a ser usado
+connection = sqlite3.connect(DB_FILE)
+#CURSOR é ele quem fará as consultas no banco de dados
+cursor = connection.cursor()
+
+
+#fechar as conexões
+cursor.close()
+connection.close()
