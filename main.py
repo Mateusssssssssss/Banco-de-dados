a80ac397f313970d3a164a9460066028ad391170
Mateus <160790022+Mateusssssssssss@users.noreply.github.com>
2024-09-06 13:17:34 -0300

    criação de tabela
    criando tabela com cursor.execute e terminado com connection.commit, para envia la, tudo isso usando o dbeaver para ver as mudanças nos codigos

--- main.py
+++ main.py
@@ -1,19 +1,31 @@
-from os import path
 import sqlite3
-
 from pathlib import Path
 
 ROOT_DIR = Path(__file__).parent
 DB_NAME = 'db.sqlite3'
 #arquivo a ser usado DB_FILE
 DB_FILE = ROOT_DIR / DB_NAME
+#tabelas de nome
+TABLE_NAME = 'customers'
 
 #Conexão, DB_FILE arquivo a ser usado
 connection = sqlite3.connect(DB_FILE)
 #CURSOR é ele quem fará as consultas no banco de dados
-cursor = connection.cursor()
+cursor = connection.cursor() 
+
+#criação de tabela
+cursor.execute(
+    f'CREATE TABLE IF NOT EXISTS {TABLE_NAME}'
+    '('
+    'id INTEGER PRIMARY KEY AUTOINCREMENT,'
+    'name TEXT,'
+    'weigth REAL'
+    ')'
+    )
+#executar o codigo
+connection.commit()
 
 
 #fechar as conexões
-cursor.close()
+cursor.close(),
 connection.close()
